6597453e8092515a1e8a012ad4d47c9d53ad28a6
Aman Kumar <aman943196@gmail.com>
2018-07-21 00:42:18 +0530

    BITCOIN: Adding Setters and Getters to ResponseBody

--- src/main/java/com/moneyTap/bitcoin/Response/ResponseBody.java
+++ src/main/java/com/moneyTap/bitcoin/Response/ResponseBody.java
@@ -11,4 +11,68 @@ public class ResponseBody {
     private long TimeFrom;
     private Boolean FirstValueInArray;
     private Conversion ConversionType;
+
+    public String getResponse() {
+        return Response;
+    }
+
+    public void setResponse(final String response) {
+        Response = response;
+    }
+
+    public String getType() {
+        return Type;
+    }
+
+    public void setType(final String type) {
+        Type = type;
+    }
+
+    public Boolean getAggregated() {
+        return Aggregated;
+    }
+
+    public void setAggregated(final Boolean aggregated) {
+        Aggregated = aggregated;
+    }
+
+    public List<Data> getData() {
+        return Data;
+    }
+
+    public void setData(final List<Data> data) {
+        Data = data;
+    }
+
+    public long getTimeTo() {
+        return TimeTo;
+    }
+
+    public void setTimeTo(final long timeTo) {
+        TimeTo = timeTo;
+    }
+
+    public long getTimeFrom() {
+        return TimeFrom;
+    }
+
+    public void setTimeFrom(final long timeFrom) {
+        TimeFrom = timeFrom;
+    }
+
+    public Boolean getFirstValueInArray() {
+        return FirstValueInArray;
+    }
+
+    public void setFirstValueInArray(final Boolean firstValueInArray) {
+        FirstValueInArray = firstValueInArray;
+    }
+
+    public Conversion getConversionType() {
+        return ConversionType;
+    }
+
+    public void setConversionType(final Conversion conversionType) {
+        ConversionType = conversionType;
+    }
 }
