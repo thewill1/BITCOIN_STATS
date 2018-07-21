6f263d7de5fac4d48629b294fc587289482c6c62
Aman Kumar <aman943196@gmail.com>
2018-07-21 15:38:07 +0530

    BITCOIN: Controller Layer

---
+++ src/main/java/com/moneyTap/bitcoin/Controllers/BitcoinController.java
@@ -0,0 +1,36 @@
+package com.moneyTap.bitcoin.Controllers;
+
+import com.moneyTap.bitcoin.CustomResponse.CustomResponseBody;
+import com.moneyTap.bitcoin.Services.BitcoinService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Timestamp;
+
+@RestController
+public class BitcoinController {
+
+    @Autowired
+    private BitcoinService bitcoinService;
+
+    @RequestMapping("/cryptoDetails")
+    public CustomResponseBody getBitcoinAdditionalDetailsUnderSpecifiedTime(
+            final @RequestParam("underSpecifiedTime") Integer underSpecifiedTime) {
+        final Timestamp requestedTimestamp = new Timestamp(System.currentTimeMillis());
+
+        return bitcoinService.getCustomeResponseBodyUnderSpecifiedTime(
+                requestedTimestamp.getTime(), underSpecifiedTime);
+    }
+
+    @RequestMapping("/sample")
+    public CustomResponseBody getSampleCustomResponseBody() {
+        final CustomResponseBody customResponseBody = new CustomResponseBody();
+
+        customResponseBody.setAverage(100d);
+        customResponseBody.setMedian(100d);
+
+        return customResponseBody;
+    }
+}
