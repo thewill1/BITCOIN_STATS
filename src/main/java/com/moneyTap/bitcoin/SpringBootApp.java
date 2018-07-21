5ee5037b2f93e30f341e7356fa9ea41a41283dbc
Aman Kumar <aman943196@gmail.com>
2018-07-21 15:40:13 +0530

    BITCOIN: Spring RestTemplate Addition

--- src/main/java/com/moneyTap/bitcoin/SpringBootApp.java
+++ src/main/java/com/moneyTap/bitcoin/SpringBootApp.java
@@ -2,10 +2,23 @@ package com.moneyTap.bitcoin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.client.RestTemplate;
 
+@ComponentScan("com.moneyTap.bitcoin")
+@PropertySource("classpath:resource.properties")
 @SpringBootApplication
 public class SpringBootApp {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootApp.class, args);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        // Do any additional configuration here
+        return builder.build();
     }
 }
